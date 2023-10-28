2575431d03eaa20a6335f3c001410f03240dd254
John Mullaney <j.a.mullaney1@gmail.com>
2023-10-27 17:16:35 -0700

    Create sample array and test function.
    It works! Will finish watching video now and correct/update.

--- LearningArrays.java
+++ LearningArrays.java
@@ -1,6 +1,6 @@
 public class LearningArrays {
     
-    int Search(int [] myarray, int lookingFor){
+    static int Search(int [] myarray, int lookingFor){
         int i;
         for(i = 0; i < myarray.length; i++){
             if (myarray[i] == lookingFor){
@@ -11,7 +11,11 @@ public class LearningArrays {
     }
     
     public static void main (String[] args){
-        
+
+        int [] simpleArray = {0, 1, 2, 3, 4, 5};
+        int indexOf2 = Search(simpleArray, 2);
+
+        System.out.println(indexOf2);
         
 
 
