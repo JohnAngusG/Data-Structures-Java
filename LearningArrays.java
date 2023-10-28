56ec76c0324906c65d6b371815f33be0b9afd4c1
John Mullaney <j.a.mullaney1@gmail.com>
2023-10-27 17:12:58 -0700

    Create first function for linear search.
    In my head the function must be called on the array. Should go through
    the entire array until you get the desired integer. Should stop and
    return index of number.

--- LearningArrays.java
+++ LearningArrays.java
@@ -1,6 +1,21 @@
 public class LearningArrays {
+    
+    int Search(int [] myarray, int lookingFor){
+        int i;
+        for(i = 0; i < myarray.length; i++){
+            if (myarray[i] == lookingFor){
+                break;
+            }
+        }
+        return i; 
+    }
+    
     public static void main (String[] args){
-        System.out.println("Hello WOrld");
+        
+        
+
+
+
     }
 
 }
