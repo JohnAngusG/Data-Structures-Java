b6714d5e41a1eba5268d3d47f08072fbbe284061
John Mullaney <j.a.mullaney1@gmail.com>
2023-10-27 17:17:47 -0700

    Change function name to Linear Search.

--- LearningArrays.java
+++ LearningArrays.java
@@ -1,6 +1,6 @@
 public class LearningArrays {
     
-    static int Search(int [] myarray, int lookingFor){
+    static int linearSearch(int [] myarray, int lookingFor){
         int i;
         for(i = 0; i < myarray.length; i++){
             if (myarray[i] == lookingFor){
@@ -13,7 +13,7 @@ public class LearningArrays {
     public static void main (String[] args){
 
         int [] simpleArray = {0, 1, 2, 3, 4, 5};
-        int indexOf2 = Search(simpleArray, 2);
+        int indexOf2 = linearSearch(simpleArray, 2);
 
         System.out.println(indexOf2);
         
