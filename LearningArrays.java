29c47082eb6461f0db34fa6480e61e62b601f3de
John Mullaney <j.a.mullaney1@gmail.com>
2023-10-27 17:40:14 -0700

    Thinking through binary search.

--- LearningArrays.java
+++ LearningArrays.java
@@ -10,6 +10,19 @@ public class LearningArrays {
         return i; 
     }
     
+    static int binarySearch(int []myarray, int needle){ // If i know the array is ordered then I can shorten the runtime. Saw it in class with Pat too. 
+        int index;                                      // Worst case scenario is need to do 20 operations since I divide the array in half each time. How to divide array in half though? Maybe assign I with the length divided by 2?
+        for (int i = myarray.length / 2; i < myarray.length; i++){
+            if (myarray[i] > needle){
+
+            }
+        }
+
+        return index;
+    }
+
+
+
     public static void main (String[] args){
 
         int [] simpleArray = {0, 1, 2, 3, 4, 5};
