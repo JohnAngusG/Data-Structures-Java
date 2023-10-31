c8b52b25a69dee4a2c4abe20dae7ba35d34d5ad8
John Mullaney <j.a.mullaney1@gmail.com>
2023-10-30 21:15:16 -0700

    Fix bug in code, wasn't swapping low and upperbounds correctly

--- OrderedArray.java
+++ OrderedArray.java
@@ -5,16 +5,53 @@ public class OrderedArray {
                 return i;
             }
             else if (array[i] > target){
-                return -1;
+                break;
             }
         }
         return -1;
     }
 
+    static int binarySearch(int [] array, int target){
+        int lowerbound = 0;
+        int upperbound = array.length - 1;
+
+        
+
+        while (lowerbound <= upperbound){
+            int midpoint = (upperbound + lowerbound) / 2;
+            int valueAtMidpoint = array[midpoint];
+
+            if (valueAtMidpoint == target) {
+                return midpoint;
+            }
+            else if (target < valueAtMidpoint){
+                upperbound = midpoint - 1;
+            }
+            else if (target > valueAtMidpoint){
+                lowerbound = midpoint + 1;
+            }
+
+
+            // if (valueAtMidpoint == target){
+            //     return midpoint;
+            // }
+            // else if (valueAtMidpoint > target){
+            //     lowerbound = midpoint + 1;
+            // }
+            // else {
+            //     upperbound = midpoint -1;
+            // }
+        }
+
+        return -1;
+    }
+
     public static void main (String[] args){
         int [] testArray = {1, 2, 3, 4, 5, 6};
-        int target = 8;
+        int target = 5;
         System.out.println(orderedArraySearch(testArray, target));
+        System.out.println(binarySearch(testArray, target));
+
     }
 
 
