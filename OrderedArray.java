3cc19d339ebac1e4ce90e973c9048b9650ccbdbb
John Mullaney <j.a.mullaney1@gmail.com>
2023-10-30 20:57:39 -0700

    Added test situation

--- OrderedArray.java
+++ OrderedArray.java
@@ -1,6 +1,21 @@
 public class OrderedArray {
-    static int orderedArraySearch(int [] array){
-        
-        return -1
+    static int orderedArraySearch(int [] array, int target){
+        for (int i = 0; i < array.length; i++){
+            if(array[i] == target){
+                return i;
+            }
+            else if (array[i] > target){
+                return -1;
+            }
+        }
+        return -1;
     }
+
+    public static void main (String[] args){
+        int [] testArray = {1, 2, 3, 4, 5, 6};
+        int target = 8;
+        System.out.println(orderedArraySearch(testArray, target));
+    }
+
+
 }
