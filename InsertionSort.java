e67bf1ca21dfc8dd0db67158f77fec124c078751
John Mullaney <j.a.mullaney1@gmail.com>
2023-11-01 18:51:43 -0700

    Add comments going through statements. Starting to make sense now.

--- InsertionSort.java
+++ InsertionSort.java
@@ -2,12 +2,14 @@ public class InsertionSort{
     // insertion sort wants to temporarily remove the value at index 1 and then shift everyhting around it. 
     
     static int [] insertionSort(int [] array){
-        for (int i = 1; i < array.length; i++){
-            int temp = array[i];
-            int position = i - 1;
 
-            while (position >= 0){
-                if (array[position] > temp){
+        for (int i = 1; i < array.length; i++){     // Going through entire length of array. 
+            int temp = array[i];                    // Taking the index 1 value and storing it in temp variable. 
+            int position = i - 1;                   // taking value TO THE LEFT of where I am initially looking at each loop. 
+
+            // this is what shift the array to the right.
+            while (position >= 0){                  // DONT UNDERSTAND THIS ONE, WHY DOES IT GO BELOW 0 EVER?
+                if (array[position] > temp){                    
                     array[position + 1] = array[position];
                     position = position - 1;
                 }
