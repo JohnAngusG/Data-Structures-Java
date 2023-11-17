1a8b7264f12868234a4141d52ec5847c2d275819
John Mullaney <j.a.mullaney1@gmail.com>
2023-11-17 09:58:33 -0800

    Remove second hashtable in function. Didn't need to create a second hashtable as the algo just goes through all of the values of one linearly anyways. Only searching through one hashmap so just make that one a hashmap.

--- IntersectionOfArrays.py
+++ IntersectionOfArrays.py
@@ -4,16 +4,13 @@
 def Interesection_Of_Arrays(array1, array2):
     solution = []
     hashArray1 = {}
-    hashArray2 = {}
 
     ## create two hashtables using arrays
     for value in array1:
         hashArray1[value] = True
-    for value in array2:
-        hashArray2[value] = True
     
-    for value in hashArray1:
-        if hashArray2.get(value):   ## tried solving it using hashArray[value] but kept getting a Key error as 1 isnt in array 2. Use get to retrieve the actual 
+    for value in array2:
+        if hashArray1.get(value):   ## tried solving it using hashArray[value] but kept getting a Key error as 1 isnt in array 2. Use get to retrieve the actual 
             solution.append(value)
     return solution
 
